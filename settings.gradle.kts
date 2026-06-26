f0dcdbab4b98e829a3c9b9c8b3b0e75d71fc74e7
André Loureiro <loureiro.andrew@gmail.com>
2026-06-26 21:05:15 +0200

    chore: update agp project version
    Signed-off-by: André Loureiro <loureiro.andrew@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,12 +9,12 @@ pluginManagement {
 plugins {
     // Build-health analyzes Android/Kotlin projects from the settings classloader.
     id("com.autonomousapps.build-health") version "3.16.0"
-    id("com.android.application") version "8.10.0" apply false
-    id("com.android.library") version "8.10.0" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
     id("com.google.dagger.hilt.android") version "2.58" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.0" apply false
 }
 
 dependencyResolutionManagement {
